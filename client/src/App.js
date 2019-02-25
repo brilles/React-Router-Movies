582bc0ace76cc662fc3acd12f2e49e15d29e3b69
Ryan Boris <ryan.boris@gmail.com>
2019-02-25 18:36:56 -0500

    Revert "implements react-router, routes"

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
-import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from 'react';
 
-import SavedList from "./Movies/SavedList";
-import MovieList from "./Movies/MovieList";
-import Movie from "./Movies/Movie";
+import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default class App extends Component {
   constructor() {
@@ -23,8 +22,7 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <Route exact path="/" component={MovieList} />
-        <Route path="/movies/:id" component={Movie} />
+        <div>Replace this Div with your Routes</div>
       </div>
     );
   }
--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,24 +1,7 @@
-import React from "react";
+import React from 'react';
 
 const MovieCard = props => {
-  return (
-    <div className="movie-card">
-      <h2>{props.movie.title}</h2>
-      <div className="movie-director">
-        Director: <em>{props.movie.director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{props.movie.metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {props.movie.stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+  return;
 };
 
 export default MovieCard;
